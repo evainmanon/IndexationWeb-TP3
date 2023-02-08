4912bf8aaf732f31d080f70fcc6283e72c82f100
ensai <evain.manon@orange.fr>
2023-02-08 12:14:12 +0100

    Ajout commentaires fonctions dans tokenize.py

--- Fonction/tokenize.py
+++ Fonction/tokenize.py
@@ -1,9 +1,11 @@
+#Function which take a word and return this word with all his caracter lower
 def lower_word(word) :
     new_word = ''
     for i in word :
         new_word = new_word + i.lower()
     return new_word
 
+#Function which take a request and return a list with the word in the request
 def list_word(request) : 
     liste_word = []
     i = 0
@@ -18,12 +20,15 @@ def list_word(request) :
             liste_word.append(request[list_esp[k]+1:list_esp[k+1]])
     return liste_word
 
+#Function which take a list and return the same list with all the word lower
 def lower_list_word(list):
     new_list = []
     for word in list :
         new_list.append(lower_word(word))
     return new_list
 
+#Function which search a word in a list.
+#If the word is in the list, we return the place of the word in the list, else, we return -1
 def search_word(word, list): 
     ind = -1 
     if list == []:
@@ -34,6 +39,7 @@ def search_word(word, list):
                 ind = i 
         return ind 
 
+#Function which take a request and return the token of this request
 def token_request(request):
     list_token = []
     list_word_request = lower_list_word(list_word(request))
